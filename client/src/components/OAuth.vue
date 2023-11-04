b1a74a44c2c82a499d673c34b9df79b81554b45e
Kowenjko <ortoswt@gmail.com>
2023-11-04 12:09:35 +0200

    added auth provider for github and microsoft

--- client/src/components/OAuth.vue
+++ client/src/components/OAuth.vue
@@ -1,27 +1,28 @@
 <script setup>
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { signInWithPopup, getAuth } from 'firebase/auth'
 import { app } from '../firebase.js'
 import { useUserStore } from '@/stores/userStore.js'
 import { useRouter } from 'vue-router'
-import { useFetch } from '@/composables/UseFetch.js'
+import { useFetch } from '@/composables/useFetch.js'
+import { useOAuth } from '@/composables/useOAuth.js'
 
 const userStore = useUserStore()
 const router = useRouter()
 
-const handleGoogleClick = async () => {
+const { getOAuthProviderIds, getOAuthProvider, getDataProvider } = useOAuth()
+
+const handleGoogleClick = async (providerId) => {
 	try {
-		const provider = new GoogleAuthProvider()
+		const provider = getOAuthProvider(providerId)
 		const auth = getAuth(app)
 
 		const result = await signInWithPopup(auth, provider)
-		const { displayName, email, photoURL } = result.user
+		const res = getDataProvider(providerId, result)
 
-		const data = await useFetch('api/auth/google', {
-			name: displayName,
-			email,
-			photo: photoURL,
-		})
+		const data = await useFetch('api/auth/google', res)
 
+		console.log('data', data)
+		if (data.success === false) return
 		userStore.setCurrentUser(data)
 		router.push('/')
 	} catch (error) {
@@ -34,9 +35,23 @@ const handleGoogleClick = async () => {
 	<button
 		type="button"
 		class="bg-red-700 text-white rounded-lg uppercase p-3 hover:opacity-95"
-		@click="handleGoogleClick"
+		@click="handleGoogleClick(getOAuthProviderIds().google)"
 	>
 		Continue with google
+	</button>
+	<button
+		type="button"
+		class="bg-[#1C2128] text-white rounded-lg uppercase p-3 hover:opacity-95"
+		@click="handleGoogleClick(getOAuthProviderIds().github)"
+	>
+		Continue with github
+	</button>
+	<button
+		type="button"
+		class="bg-[#0078D4] text-white rounded-lg uppercase p-3 hover:opacity-95"
+		@click="handleGoogleClick(getOAuthProviderIds().microsoft)"
+	>
+		Continue with microsoft
 	</button>
 </template>
 
