8fd2e9ef8882398081ccb82d3eba63382c0280b4
Kowenjko <ortoswt@gmail.com>
2023-11-06 13:33:26 +0200

    complete create listing page functionality

--- client/src/pages/CreateListing.vue
+++ client/src/pages/CreateListing.vue
@@ -1,12 +1,34 @@
 <script setup>
 import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
 import { uploadFileStorage } from '@/api/repository/StorageRepository.js'
 import { getDownloadURL } from 'firebase/storage'
+import { useFetch } from '@/composables/UseFetch.js'
+import { useUserStore } from '@/stores/userStore.js'
 
 const files = ref([])
 const imageUploadError = ref(null)
-const formData = reactive({ imageUrls: [] })
+const error = ref(null)
+const loading = ref(false)
 const uploading = ref(false)
+
+const formData = reactive({
+	imageUrls: [],
+	name: 'test',
+	description: 'dfdf',
+	address: 'df',
+	type: 'rent',
+	bedrooms: 1,
+	bathrooms: 1,
+	regularPrice: 50,
+	discountPrice: 0,
+	offer: false,
+	parking: true,
+	furnished: false,
+})
+
+const userStore = useUserStore()
+const router = useRouter()
 
 const handleChangeFiles = (event) => {
 	files.value = event.target.files
@@ -63,13 +85,41 @@ const handleImageSubmit = async () => {
 const handleRemoveImage = (index) => {
 	formData.imageUrls = [...formData.imageUrls].filter((_, i) => i !== index)
 }
+
+const handleSubmit = async () => {
+	if (formData.regularPrice < formData.discountPrice)
+		return (error.value = 'Discount price must be lower than regular price')
+
+	if (formData.imageUrls.length < 1)
+		return (error.value = 'You must upload at least one image')
+
+	try {
+		loading.value = true
+		const data = await useFetch(`api/listing/create`, {
+			...formData,
+			userRef: userStore.currentUser._id,
+		})
+
+		if (data?.success === false) {
+			return (error.value = data.message)
+		}
+		router.push(`/listing/${data._id}`)
+	} catch (error) {
+		error.value = error.message
+	} finally {
+		loading.value = false
+	}
+}
 </script>
 
 <template>
 	<main class="max-w-4xl mx-auto p-3">
 		<h1 class="text-3xl font-semibold text-center my-7">Create Listing</h1>
 
-		<form class="flex flex-col sm:flex-row gap-4" @submit.prevent="">
+		<form
+			class="flex flex-col sm:flex-row gap-4"
+			@submit.prevent="handleSubmit"
+		>
 			<div class="flex flex-col gap-4 w-full flex-1">
 				<input
 					type="text"
@@ -80,11 +130,13 @@ const handleRemoveImage = (index) => {
 					minlength="10"
 					required
 					autocomplete="off"
+					v-model="formData.name"
 				/>
 				<textarea
 					placeholder="Description"
 					class="border p-3 rounded-lg"
 					id="description"
+					v-model="formData.description"
 					required
 				/>
 				<input
@@ -94,26 +146,57 @@ const handleRemoveImage = (index) => {
 					id="address"
 					required
 					autocomplete="off"
+					v-model="formData.address"
 				/>
+
 				<div class="flex gap-6 flex-wrap">
-					<div class="flex gap-2 items-center">
-						<input type="checkbox" id="sale" class="w-5 cursor-pointer" />
-						<label for="sale" class="cursor-pointer">Sell</label>
+					<div class="flex gap-6 border border-gray-300 p-2 rounded">
+						<div class="flex gap-2 items-center">
+							<input
+								type="radio"
+								id="sale"
+								class="w-5 cursor-pointer"
+								value="sale"
+								v-model="formData.type"
+							/>
+							<label for="sale" class="cursor-pointer">Sale</label>
+						</div>
+						<div class="flex gap-2 items-center">
+							<input
+								type="radio"
+								id="rent"
+								class="w-5 cursor-pointer"
+								value="rent"
+								v-model="formData.type"
+							/>
+							<label for="rent" class="cursor-pointer">Rent</label>
+						</div>
 					</div>
 					<div class="flex gap-2 items-center">
-						<input type="checkbox" id="rent" class="w-5 cursor-pointer" />
-						<label for="rent" class="cursor-pointer">Rent</label>
-					</div>
-					<div class="flex gap-2 items-center">
-						<input type="checkbox" id="parking" class="w-5 cursor-pointer" />
+						<input
+							type="checkbox"
+							id="parking"
+							class="w-5 cursor-pointer"
+							v-model="formData.parking"
+						/>
 						<label for="parking" class="cursor-pointer">Parking spot</label>
 					</div>
 					<div class="flex gap-2 items-center">
-						<input type="checkbox" id="furnished" class="w-5 cursor-pointer" />
+						<input
+							type="checkbox"
+							id="furnished"
+							class="w-5 cursor-pointer"
+							v-model="formData.furnished"
+						/>
 						<label for="furnished" class="cursor-pointer">Furnished</label>
 					</div>
 					<div class="flex gap-2 items-center">
-						<input type="checkbox" id="offer" class="w-5 cursor-pointer" />
+						<input
+							type="checkbox"
+							id="offer"
+							class="w-5 cursor-pointer"
+							v-model="formData.offer"
+						/>
 						<label for="offer" class="cursor-pointer">Offer</label>
 					</div>
 				</div>
@@ -126,6 +209,7 @@ const handleRemoveImage = (index) => {
 							max="10"
 							required
 							class="p-3 border-gray-300 rounded-lg"
+							v-model="formData.bedrooms"
 						/>
 						<label for="bedrooms">Beds</label>
 					</div>
@@ -137,6 +221,7 @@ const handleRemoveImage = (index) => {
 							max="10"
 							required
 							class="p-3 border-gray-300 rounded-lg"
+							v-model="formData.bathrooms"
 						/>
 						<label for="bathrooms">Baths</label>
 					</div>
@@ -144,24 +229,27 @@ const handleRemoveImage = (index) => {
 						<input
 							type="number"
 							id="regularPrice"
-							min="1"
-							max="10"
+							min="50"
+							max="1000000"
 							required
 							class="p-3 border-gray-300 rounded-lg"
+							v-model="formData.regularPrice"
 						/>
 						<label for="regularPrice" class="flex flex-col items-center">
 							Regular price
 							<span class="text-xs">($ / Month)</span>
 						</label>
 					</div>
-					<div class="flex items-center gap-2">
+
+					<div v-if="formData.offer" class="flex items-center gap-2">
 						<input
 							type="number"
 							id="discountPrice"
-							min="1"
-							max="10"
+							min="0"
+							max="1000000"
 							required
 							class="p-3 border-gray-300 rounded-lg"
+							v-model="formData.discountPrice"
 						/>
 						<label for="discountPrice" class="flex flex-col items-center">
 							Discounted price
@@ -222,9 +310,11 @@ const handleRemoveImage = (index) => {
 				<button
 					type="submit"
 					class="uppercase p-3 bg-slate-700 rounded-lg text-white hover:opacity-95 disabled:opacity-80"
+					:disabled="loading || uploading"
 				>
-					Create Listing
+					{{ loading ? 'Creating...' : 'Create Listing' }}
 				</button>
+				<p v-if="error" class="text-red-700 text-sm">{{ error }}</p>
 			</div>
 		</form>
 	</main>
